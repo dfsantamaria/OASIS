71709fccbea5f7c3715882e98ff132332820fdcb
Daniele Francesco Santamaria <dfsantamaria@users.noreply.github.com>
2023-05-07 08:36:03 +0200

    Add files via upload

---
+++ OASIS-MAN/Python/source/OASIS-Tbox-Generator.py
@@ -0,0 +1,37 @@
+from rdflib import *
+
+
+def __init__():
+    # IRI
+    oasis_iri = "https://raw.githubusercontent.com/dfsantamaria/OASIS/v2/OASIS-ontologies/oasis.owl"
+    tbox_iri = "http://www.dmi.unict.it/oasis-tbox.owl"
+
+    # Namespaces
+    tbox = Namespace(tbox_iri+"#")
+    oasis = Namespace("http://www.dmi.unict.it/oasis.owl")
+
+    # Ontology
+    ontology = Graph(base=tbox)
+    ontology.namespace_manager.bind("base", tbox)
+    ontology.namespace_manager.bind("oasis", oasis)
+
+    ontology.add((URIRef(tbox), RDF.type, OWL.Ontology))
+
+    # Classes
+
+    # ObjectProperties
+
+    # Imports
+    # Remember to add manually the import
+    ontology.add((URIRef(tbox), OWL.imports, URIRef(oasis_iri)))
+    # ontology.add((URIRef(god_iri), OWL.imports, URIRef(geo_iri)))
+
+    # Save
+    file = open("oasis-tbox.owl", "w")
+    file.write(ontology.serialize(format='pretty-xml'))
+
+
+# end __init__
+
+if __name__ == '__main__':
+    __init__()
